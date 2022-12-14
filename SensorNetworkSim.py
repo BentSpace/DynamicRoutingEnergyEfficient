0904a56067566858e3d866fbf3998988e1d6ae07
Nathan Bertram <bentspacetime@gmail.com>
2022-12-14 13:15:20 -0800

    Simulation Runs

--- SensorNetworkSim.py
+++ SensorNetworkSim.py
@@ -12,6 +12,8 @@
 # (1 / powerRemainingNode + 1 / powerRemainingNode + ...(for each node in path))
 # from IPython.display import display, HTML
 import pandas as pd
+import time
+import random
 
 # each node is a sensor.  gw is the gateway where the sensors need to send their data
 # sensorGraph = {
@@ -27,145 +29,6 @@ import pandas as pd
 #     "j" : ["e", "b"]
 # }
 
-
-
-# # the actual power remaining in each sensor node
-# actualPowerRemaining = {
-#     "a" : 100,
-#     "b" : 100,
-#     "c" : 100,
-#     "gw" : 100
-#     # "d" : 100,
-#     # "e" : 100,
-#     # "f" : 100,
-#     # "g" : 100,
-#     # "h" : 100,
-#     # "i" : 100,
-# }
-
-
-# the distance vector maintained by each node, which is their estimate of the 
-# cost to reach every other node. The first level is the node the table belongs
-# to.  The second level is the "from" node, for each of the node's nieghboors.
-# The value in the key is the estimate distance from v to the gateway.  To save
-# memory and energy, only need to compute distance to gateway, since that is the
-# only destination.
-# # the distance vector maintained by each node, which is their estimate of the 
-# # cost to reach every other node. The first level is the node the table belongs
-# # to.  The second level is the "from" node.  The bottom level keys are the "to"
-# # nodes.
-# nodeTables = {}
-#     "a" : {
-#         "a" :  {
-#             "a" : 0,
-#             "b" : 1/100,
-#             "c" : 1/100,
-#             "gw" : 10000
-#         },
-#         "b" : {
-#             "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "c" : {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         }
-#     },
-#     "b" : {
-#         "a" :  {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "b" : {
-#             "a" : 1/100,
-#             "b" : 0,
-#             "c" : 10000,
-#             "gw" : 1/100
-#         },
-#         "c" : {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         }
-#     },
-#     "c" : 
-#     {
-#         "a" :  {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "b" : {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "c" : {
-#             "a" : 1/100,
-#             "b" : 10000,
-#             "c" : 0,
-#             "gw" : 1/100
-#         }
-#     },
-# }
-
-
-# # Cost of the link from node x to node v.
-# def c(x,v):
-#     if x == v:
-#         return 0
-#     elif v in sensorGraphSimple[x]:
-#         return 1 / actualPowerRemaining[v]
-#     else:
-#         return 10000
-
-# # Distance vector update algorithm. Estimated cost from node x to y.
-# def D(x,y):
-#     if x == y:
-#         return 0
-#     else:
-#         mini = 10000
-#         # for each neighbor v of x
-#         for v in sensorGraphSimple[x]:
-#             result = c(x,v) + D(v,y) 
-#             if result < mini:
-#                 mini = result
-#         return mini
-
-# # initializes the forwarding tables for a sensor node x
-# def initialization(x):
-#     # create and add x's DV to it's table
-#     distanceVector = {}
-#     # create empty dict to hold x's forward table
-#     nodeTables[x] = {}
-#     for y in N:
-#         distanceVector[y] = c(x,y)
-#     global NodeTables
-#     nodeTables[x][x] = distanceVector
-
-#     # set distance vectors of x's neighbors (w) to infinity and add to x's table
-#     for w in sensorGraphSimple[x]:
-#         distanceVector = {}
-#         for y in N:
-#             distanceVector[y] = 10000
-#         nodeTables[x][w] = distanceVector
-
-
-# for x in N:
-#     initialization(x)
-# time = 0
-# # print(c("a","gw"))
-# D("a","gw")
-
 # An individual sensor node in the network
 class sensorNode:
     sensorGraphSimple = {
@@ -177,54 +40,109 @@ class sensorNode:
     N = list(sensorGraphSimple.keys()) # list of Nodes in network
     def __init__(self, name) -> None:
         self.name = name
-        self.powerRemaining = 100
-        # The rows in the forwarding table are the distance vectors of itself 
+        self.powerRemaining = 10
+        self.vStarGW = None # The next hop on the least cost path to the gateway
+
+        # The rows in the routing table are the distance vectors of itself 
         # and it's neighbooring nodes.  
         # The columns contain the estimated cost to reach that node in the column
         # label, from the node in the row label
         dfIndex = list(self.sensorGraphSimple[self.name])
         dfIndex.insert(0, self.name)
-        self.forwardingTable = pd.DataFrame(index=dfIndex, columns=self.N)
-        # self.forwardingTable.columns = self.N
+        self.routingTable = pd.DataFrame(index=dfIndex, columns=self.N)
         self.neighborsPowerRemaining = {}
         for v in self.sensorGraphSimple[self.name]:
             self.neighborsPowerRemaining[v] = 100
-        # print(self.neighborsPowerRemaining)
-        self.forwardingTable = self.forwardingTable.fillna(10000)
+        self.routingTable = self.routingTable.fillna(10000)
+
         # construct own DV, to start need assume nodes fully charged
         for v in self.sensorGraphSimple[self.name]:
-            self.forwardingTable.at[self.name, v] = 1/100
-        self.forwardingTable = self.forwardingTable.fillna(10000)
+            self.routingTable.at[self.name, v] = 1/100
+        self.routingTable = self.routingTable.fillna(10000)
 
         # set cells with matching index and label to 0
         for x in dfIndex:
             for y in self.N:
                 if x == y:
-                    self.forwardingTable.at[x,y] = 0
-        # print(self.forwardingTable)
-
-
-
+                    self.routingTable.at[x,y] = 0
      
     # Cost of the link from node x to node v.
     def c(self,x,v):
         if x == v:
             return 0
-        elif v in self.neighborsPowerRemaining[v]:
+        elif v in self.neighborsPowerRemaining.keys():
             return 1 / self.neighborsPowerRemaining[v]
         else:
             return 10000
 
     # Sends own DV to v
     def sendDV(self, v):
-        ownDV = self.forwardingTable.iloc[0]
-        sensorNetworkSim.dictOfSensorObjects[v].receiveDV(self.name, ownDV)
+        self.decrementPower()
+        ownDV = self.routingTable.iloc[0]
+        print("sendDV", self.name, " -> ", v)
+        sensorNetworkSim.dictOfSensorObjects[v].receiveDV(self.name, ownDV, self.powerRemaining)
 
     # For receiveing a DV from node x
-    def receiveDV(self, x, DV):
-        self.forwardingTable.loc[x] = DV
-        print(self.forwardingTable)
+    def receiveDV(self, x, DV, powerLevel):
+        # if the incoming DV is diferrent
+        oldDV = self.routingTable.loc[x]
+        self.neighborsPowerRemaining[x] = powerLevel
+        if not oldDV.equals(DV):
+            self.routingTable.loc[x] = DV
+            self.updateOwnDV()
         
+    def updateOwnDV(self):
+        newDV = []
+        for y in self.N:
+            result = self.D(self.name, y)
+            newDV.append(result)
+        self.routingTable.loc[self.name] = newDV
+
+    # Distance vector update algorithm. Estimated cost from node x to y.
+    def D(self,x,y):
+        if x == y:
+            return 0
+        else:
+            mini = 10000
+            vStar = None
+            # for each neighbor v of x
+            for v in self.sensorGraphSimple[x]:
+                result = self.c(x,v) + self.routingTable.at[v,y] 
+                if result < mini:
+                    mini = result
+                    vStar = v
+            # if calculating path to gw, update vStarGW
+            if y == "gw":
+                self.vStarGW = vStar
+            return mini
+
+    def nodeDead(self):
+        sensorNetworkSim.nodeDead = True
+
+    def sendPacket(self):
+        packetNum = random.randrange(1, 1000)
+        data = random.randrange(1, 100)
+        self.decrementPower()
+        sensorNetworkSim.dictOfSensorObjects[self.vStarGW].receivePacket(packetNum, data)
+        print("sendPacket#: ", packetNum, "data: ", data,  self.name, " -> ", self.vStarGW)
+        
+        
+    def receivePacket(self, packetNum, data):
+        if self.name != "gw":
+            self.relayPacket(packetNum, data)
+
+    def relayPacket(self, packetNum, data):
+        self.decrementPower()
+        sensorNetworkSim.dictOfSensorObjects[self.vStarGW].receivePacket(packetNum, data)
+        print("sendPacket#: ", packetNum, "data: ", data,  self.name, " -> ", self.vStarGW)
+
+    def decrementPower(self):
+        if self.name != "gw":
+            self.powerRemaining = self.powerRemaining - 1
+            print("node", self.name, "powerRemaining",self.powerRemaining)
+        if self.powerRemaining == 0:
+            self.nodeDead()
+
 class sensorNetworkSim:
     sensorGraphSimple = {
     "a" : ["b", "c"],
@@ -234,6 +152,7 @@ class sensorNetworkSim:
     }
     N = list(sensorGraphSimple.keys())
     dictOfSensorObjects = {}
+    nodeDead = False
 
     def __init__(self) -> None:
         self.initSensorNodes()
@@ -248,75 +167,18 @@ class sensorNetworkSim:
         for x in self.N:
             for v in self.sensorGraphSimple[x]:
                 self.dictOfSensorObjects[x].sendDV(v)
-    
+        self.runSimulation()
+
+    def runSimulation(self):
+        start = time.time()
+        while not self.nodeDead:
+            # sensors send out packets every 1 second
+            for sensor in self.dictOfSensorObjects.values():
+                if sensor.name != "gw":
+                    sensor.sendPacket()
+            time.sleep(1)
+        end = time.time()   
+        print("Network stayed alive for: ", end - start, "seconds")
+
 if __name__ == '__main__':
     sensorNetworkSim()
-   
-
-# # the distance vector maintained by each node, which is their estimate of the 
-# # cost to reach every other node. The first level is the node the table belongs
-# # to.  The second level is the "from" node.  The bottom level keys are the "to"
-# # nodes.
-# nodeTables = {
-#     "a" : {
-#         "a" :  {
-#             "a" : 0,
-#             "b" : 1/100,
-#             "c" : 1/100,
-#             "gw" : 10000
-#         },
-#         "b" : {
-#             "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "c" : {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         }
-#     },
-#     "b" : {
-#         "a" :  {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "b" : {
-#             "a" : 1/100,
-#             "b" : 0,
-#             "c" : 10000,
-#             "gw" : 1/100
-#         },
-#         "c" : {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         }
-#     },
-#     "c" : 
-#     {
-#         "a" :  {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "b" : {
-#            "a" : 10000,
-#             "b" : 10000,
-#             "c" : 10000,
-#             "gw" : 10000
-#         },
-#         "c" : {
-#             "a" : 1/100,
-#             "b" : 10000,
-#             "c" : 0,
-#             "gw" : 1/100
-#         }
-#     },
-# }
